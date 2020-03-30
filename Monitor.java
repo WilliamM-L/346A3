19c72cf0fe213a78ad7054063de7df45ee394fd0
CamilBouzidi <camilbouzidi@hotmail.com>
2020-03-30 14:58:22 -0400

    Preliminary Talking Work
    Added early logic for the talking part of the monitor. Algo might be wrong (especially since the wait method picks up where it left off), please take a look at it

--- Monitor.java
+++ Monitor.java
@@ -14,6 +14,7 @@ public class Monitor
 	 * ------------
 	 */
 	int numPhil;
+	//tag urself
 	enum State {THINKING, EATING, TALKING, HUNGRY};
 	State[] philState;
 
@@ -48,11 +49,14 @@ public class Monitor
 		//CAMIL: Have to add a check for tryEating at the beginning of this method.
 		philState[piTID] = State.HUNGRY;
 		//Every one around me is eating, then I have to wait
-		try {
-			wait();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		tryEating(piTID);
+		if (philState[piTID] != State.EATING) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				System.err.println(e);
+			}
 		}
 		
 	}
@@ -64,24 +68,54 @@ public class Monitor
 	public synchronized void putDown(final int piTID)
 	{
 		// ...
+		philState[piTID] = State.THINKING;
+		tryEating(((piTID)+(numPhil-1))%5);
+		tryEating((piTID+1)%5);
 	}
 
 	/**
 	 * Only one philopher at a time is allowed to philosophy
 	 * (while she is not eating).
 	 */
-	public synchronized void requestTalk()
+	public synchronized void requestTalk(final int piTID)
 	{
-		// ...
+		//Extremely unsure about the logic but here goes:
+		//separating it in 2 for loops ensures that all philosophers are checked, no matter
+		//which philosopher does it first.
+		//Please verify this!!!!!
+		//It probably isn't right because the when you wait,
+		//once the waiting is done you go back directly to the line after
+		if (philState[piTID] != State.EATING) {
+			for (int i=0; i<piTID; i++) {
+				if (philState[i]==State.TALKING) {
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						System.err.println(e);
+					}
+				}
+			}
+			for (int j=piTID; j<numPhil; j++) {
+				if (philState[j]==State.TALKING) {
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						System.err.println(e);
+					}
+				}
+			}
+			philState[piTID] = State.TALKING;
+		}
 	}
 
 	/**
 	 * When one philosopher is done talking stuff, others
 	 * can feel free to start talking.
 	 */
-	public synchronized void endTalk()
+	public synchronized void endTalk(final int piTID)
 	{
-		// ...
+		//This needs extra logic to make sure we're letting every other thread try to talk
+		philState[piTID] = State.THINKING;
 	}
 	//CAMIL: I'm the one who added this method.
 	public synchronized void tryEating(final int piTID) {
